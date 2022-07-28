41e6cebef5bb2cf347f0c3d72c938cc7a752c6ea
Jessica Classon <jessicaclasson4@gmail.com>
2022-07-28 08:32:11 -0600

    Update createaccount.js

--- public/createaccount.js
+++ public/createaccount.js
@@ -1,6 +1,7 @@
 function CreateAccount(){
 const [show, setShow]    =React.useState(true);
 const [status, setStatus]=React.useState('');
+const [user, setUser]    = React.useState(' ');
 
 return(
     <Card
@@ -8,39 +9,56 @@ return(
        header="Create Account"
        status={status}
        body={show ? 
-       <CreateForm setShow={setShow}/> :
-       <CreateMsg setShow={setShow}/>}
+       <CreateForm setShow={setShow} setStatus={setStatus} setUser={setUser}/> :
+       <CreateMsg setShow={setShow} setStatus={setStatus} user={user}/>}
     />
 )
 }
 
 function CreateMsg(props){
+  const bankUser = props.user.email;
 return(
 <div>
-  <h5>Success</h5>
+  <h5>{`Account created for: ${bankUser}`}</h5>
   <button type="submit"
     className="btn btn-light"
-    onClick={() => props.setShow(true)}>Add Another Account</button>    
+    onClick={() => props.setShow(true)}>Add Another Account</button>  
 </div>);
 }
 
-function CreateForm(props) {
+function CreateForm(props){
 const [name, setName]         =React.useState('');
 const [email, setEmail]       =React.useState('');
 const [password, setPassword] =React.useState('');
-
+const ctx = React.useContext(UserContext);
+const user = ctx.user;
 
 // Push new user into the ctx
 function handle() {
-console.log(name,email,password);
-const url = `/account/create/${name}/${email}/${password}`;
-( async () => {
-  var res  = await fetch(url);
-  var data = await res.json();
-  console.log(data)
-})();
-props.setShow(false);
-}
+console.log(email,password);
+firebase
+  .auth()
+  .createUserWithEmailAndPassword(email, password)
+  .then((userMade) => {
+    const user = userMade.user;
+    console.log(`User has been created: ${user.email}`);
+    props.setUser(user);
+    props.setShow(false);
+    props.setStatus('');
+  })
+  .catch((err) => {
+    const errMsg = err.message;
+    console.log(`err:  ${errMsg}`);
+  });
+  const url = `/account/create/${name}/${email}/${password}`;
+  (async () => {
+      var res  = await fetch(url);
+      var data = await res.json();    
+      console.log(data);        
+  })();
+  ctx.user = { name: name, email: email, password: password};
+  props.setShow(false);
+}   
 
 return(
 <div>
