b49e21c88fedb2925b47d2f261835781abd7e414
Jessica Classon <jessicaclasson4@gmail.com>
2022-07-28 08:40:17 -0600

    update withdraw.js

--- public/withdraw.js
+++ public/withdraw.js
@@ -1,66 +1,72 @@
-function Withdraw(){
+function Withdraw(props){
     const [show, setShow]    =React.useState(true);
     const [status, setStatus]=React.useState('');
-    const [balance, setBalance] =React.useState('');
+    const [amount, setAmount] = React.useState('');
     
     return(
-        <Card
-           bgcolor="dark"
-           header="Withdraw"
-           status={status}
-           body={show ? 
-           <WithdrawForm setShow={setShow} setStatus={setStatus}/> :
-           <WithdrawMsg setShow={setShow} setStatus={setStatus}/>}
-        />
-    )
+      <Card
+      bgcolor="dark"
+      header="Withdraw"
+      status={status}
+      body={show ? 
+        (<>
+        <WithdrawForm user={props.user} setShow={setShow} setStatus={setStatus} setAmount={setAmount}/> 
+       </> ):
+       <> <WithdrawMsg setShow={setShow} setStatus={setStatus} amount={amount}/>
+    </> }
+    />
+  )
 }
+
 
 function WithdrawMsg(props){
-  const ctx = React.useContext(UserContext);
-    return(
-    <>
-      <h5> Successful withdraw</h5>
-      Balance: {ctx.users[0].balance}
-      <button type="submit"
-        className="btn btn-light"
-        onClick={() => {
+  return (<>
+    <h5>Successful withdraw</h5>
+    <button type="submit" 
+      className="btn btn-light" 
+      onClick={() => {
           props.setShow(true);
           props.setStatus('');
-        }}>
-          Make another withdraw
-      </button>
-    </>);
-}
+      }}>
+        Make another withdraw
+    </button>
+  </>);
+} 
 
 function WithdrawForm(props) {
-    const [email, setEmail]       =React.useState('');
+    const [email, setEmail]   = React.useState('');
     const [amount, setAmount] = React.useState('');
+    const [balance, setBalance] =React.useState('');
     const ctx = React.useContext(UserContext);
 
 // Push new user into the ctx
 function handle() {
-  fetch(`/account/withdraw/${email}/-${amount}`)
+  fetch(`/account/update/${email}/-${amount}`)
   .then(response => response.text())
   .then(text => {
-      try {
-        const data = JSON.parse(text);
-        props.setShow(false);
-        console.log('JSON:', data);
-      } catch(err) {
-          props.setStatus('Withdraw failed')
-          console.log('err:', text);
-      }
-  });
+    try {
+      const data = JSON.parse(amount);
+      ctx.balance -= amount;
+      props.setStatus(data.amount);
+      props.setShow(false);
+      props.setAmount(data.amount);
+      window.alert(`You withdrew ${amount}!`);
+      console.log("JSON:", data)
+  } catch(err) {
+      props.setStatus('Withdrew failed')
+      console.log('err:', text);
+  }
+});
 }
 
 return(
 <div>
-Email<br/>
-  <input type="input" 
-  className="form-control" 
-  placeholder="Enter email" 
-  value={email} 
-  onChange={e => setEmail(e.currentTarget.value)}/><br/>
+Email Address:<br/>
+<input type="input" 
+className="form-control" 
+placeholder="Enter email" 
+value={email} 
+onChange={e => setEmail(e.currentTarget.value)}/><br/>
 
 Amount<br/>
 <input type='Number'
@@ -71,7 +77,6 @@ Amount<br/>
 
 <button type='submit'
    className='btn btn-light'
-   balance={ctx.users[0].balance} 
    onClick={handle}>Withdraw</button> 
 </div>);
 }
